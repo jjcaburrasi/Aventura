249d084bef48ba8fa2aa0a3e367433e5c5d3627c
Jose Caburrasi <jjcaburrasi@gmail.com>
2022-05-01 00:04:55 +0200

    avanzando en demo

--- Aventura.py
+++ Aventura.py
@@ -2,15 +2,119 @@ inventario=[]
 traje=False
 chandal=False
 ducha=False
+oporatsistemas=True
+oporjardinero=True
+oporserviciotecnico=False
 dia=1
 dinero=0
-hora="6:30"
+hora="6:00"
+def linkedin():
+	global oporserviciotecnico
+	print("Estas en Linkedin, entre unas cuantas publicaciones de vendehumos, ves una publicacion de Jose Miguel Garcia Muñiz")
+	print("Este hombre siempre es sinonimo de calidad")
+	print("Puedes MIRAR OFERTAS, MIRAR MENSAJES o SALIR")
+	respuesta = input (">")
+	while respuesta != ("SALIR")
+		if respuesta == ("MIRAR OFERTAS"):
+			print("Hay una oferta de helpdesk")
+			print("¿Quieres aplicar a la oferta?")
+			respuesta=("SI/NO>")
+				if respuesta == ("SI")
+					print("Mandas el CV, has hecho esto tantas veces que ya ni te ilusiona")
+				elif respuesta ==("NO")
+					oporserviciotecnico=True
+				else:
+					print("No te entiendo")
+		elif respuesta == ("MIRAR MENSAJES"):
+			if dia==(1)
+				print("No tienes mensajes nuevos")
+			elif dia == (2) and oporserviciotecnico == True:
+				print("Te han contactado de la oferta de helpdesk, te piden que llames al numero 600-195-596")
+		else:
+			print("No te entiendo")
+	ordenador()
+def atsistemas():
+	pass
 def procastinar():
-	pass
+	global hora
+	print("Te pones a jugar al buscaminas.")
+	print("¿Hasta que hora quieres jugar?")
+	print("Las horas deben ser en punto y entre 00:00 y 23:00")
+	respuesta = input(">")
+	if respuesta > ("23:00") or respuesta < ("00:00"):
+		print("Hora incorrecta")
+	else:
+		hora = respuesta
+		print("Ahora son las " + hora)
+
 def movil():
-	pass
+	print("Puedes mirar la AGENDA, LLAMAR o SALIR")
+def skype():
+	global oporatsistemas
+	global hora
+	global dia
+	global ducha
+	print("Estas en Skype.")
+	print("Escribe SALIR para volver a la seleccion de programa")
+	if hora != ("10:00") or dia != (1) or oporatsistemas==False:
+		print ("Abres el programa, no tienes ningun contacto, hace tiempo que te pasaste a Discord, pero algunas empresas aun prefieren hacer las entrevistas por Skype")
+		respuesta=input("¿Que quieres hacer ahora, SALIR o CONTINUAR EN SKYPE?>")	
+		if respuesta ==("SALIR"):
+			ordenador()
+		elif respuesta== ("CONTINUAR EN SKYPE"):
+			skype()
+		else:
+			print("No te entiendo")
+	elif hora ==("10:00") and dia ==(1) and oporatsistemas==True:
+		print ("Tienes una llamada entrante, es Elena Cantero, responsable de seleccion de Atsistemas")
+		print ("¿Quieres responder?")
+		respuesta=input("SI/NO>")
+		if respuesta == ("SI"):
+			atsistemas()
+		elif respuesta == ("NO"):
+			print("Pierdes la oportunidad de trabajar en Atsistemas")
+			oporatsistemas=False
+			respuesta=input("¿Que quieres hacer ahora, SALIR o CONTINUAR EN SKYPE?>")	
+			if respuesta ==("SALIR"):
+				ordenador()
+			elif respuesta== ("CONTINUAR EN SKYPE"):
+				skype()
+			else:
+				print("No te entiendo")
+		else:
+			print("No te entiendo")
+			respuesta=input("¿Que quieres hacer ahora, SALIR o CONTINUAR EN SKYPE?>")	
+			if respuesta ==("SALIR"):
+				ordenador()
+			elif respuesta== ("CONTINUAR EN SKYPE"):
+				skype()
+			else:
+				print("No te entiendo")	
 def ordenador():
-	pass
+	
+	print("Te sientas delante del ordenador y te crujes los dedos, has visto en muchas peliculas que eso ayuda a usarlo")
+	print("Puedes abrir SKYPE, LINKEDIN o GMAIL, tambien puedes SALIR")
+	respuesta=input(">")
+	while respuesta != ("SALIR"):
+		if respuesta==("ABRIR SKYPE"):
+			skype()
+		elif respuesta==("ABRIR LINKEDIN"):
+			linkedin()
+		elif respuesta==("ABRIR GMAIL"):
+			
+		elif respuesta==("INVENTARIO"):
+			inv()
+		elif respuesta==("ESTADO"):
+			estado()
+		elif respuesta==("PROCASTINAR"):
+			procastinar()
+		elif respuesta==("DESCRIPCION"):
+			print("Te sientas delante del ordenador y te crujes los dedos, has visto en muchas peliculas que eso ayuda a usarlo")
+			print("Puedes abrir SKYPE, LINKEDIN o FACEBOOK, tambien puedes SALIR")
+		else:
+			print("No te entiendo")	
+		respuesta=input(">")
+	oficina()
 def estado():
 	print("Estas en el día " + str(dia))
 	print("Son las "+ hora)
@@ -29,7 +133,7 @@ def instrucciones():
 	print("LA BUSQUEDA")
 	print("===========")
 	print("Tu nombre es Paco Delcán, llevas en paro 1 año, y ya la situación es insostenible")
-	print("Tienes una semana para encontrar trabajo antes de que pierdas la cabeza")
+	print("Tienes tres dias para encontrar trabajo antes de que pierdas la cabeza")
 	print("ESTO ES UNA DEMO, UNA MUESTRA DEL JUEGO, EN ESTA DEMO PODRÁS ENCONTRAR 3 TRABAJOS DIFERENTES")
 	print("")
 	print("CÓMO JUGAR")
@@ -84,6 +188,8 @@ def baño():
 			inv()
 		elif respuesta==("ESTADO"):
 			estado()
+		elif respuesta==("PROCASTINAR"):
+			procastinar()
 		elif respuesta==("DESCRIPCION"):
 			print("Estas en el cuarto de baño, y bueno, aqui pasas el tiempo que no estas en la oficina")
 			print("Hay una DUCHA, puedes ir al PASILLO")
@@ -145,6 +251,8 @@ def cocina():
 			inv()
 		elif respuesta==("ESTADO"):
 			estado()
+		elif respuesta==("PROCASTINAR"):
+			procastinar()
 		elif respuesta==("DESCRIPCION"):
 			print("Es una cocina estilo moderno, te encantaria tener una isla central, pero el espacio es limitado")
 			print("Hay una NEVERA, puedes ir al SALON")
@@ -171,6 +279,8 @@ def salon():
 			inv()
 		elif respuesta==("ESTADO"):
 			estado()
+		elif respuesta==("PROCASTINAR"):
+			procastinar()
 		elif respuesta==("DESCRIPCION"):
 			print("Sales al salón, esta todo desordenado")
 			print("En una esquina hay un extraño ARTILUGIO, puedes ir a la COCINA o al PASILLO")
@@ -192,6 +302,8 @@ def inv():
 			print("Te permite usar los autobuses de línea")
 		elif respuesta==("MIRAR TARJETA BANCARIA"):
 			print("Es una tarjeta de débito")
+		elif respuesta==("MIRAR MOVIL") or respuesta==("USAR MOVIL"):
+			movil()
 		else:
 			print("No te entiendo")
 			print("Puedes MIRAR los objetos que tienes, o bien SALIR del inventario")
@@ -208,7 +320,16 @@ def dormitorio():
 		if respuesta==("ABRIR MESITA"):
 			print("Hay una caja de CASETTE vacia, recuerdo en mi infancia que los juegos de MSX venian en este formato")
 		elif respuesta==("MIRAR MESITA"):
-			print("Es la tipica mesita de noche")
+			print("Es la tipica mesita de noche, tienes el MOVIL encima")
+		elif respuesta==("MIRAR MOVIL"):
+			print("Es un Xiaomi, lo compraste hace poco, pero ya tiene la pantalla rota, eres un desastre")
+		elif respuesta==("COGER MOVIL"):
+			if ("MOVIL") in inventario:
+				print("Ya lo tienes")
+			else:
+				print("Coges tu MOVIL, ahora lo encontraras en el INVENTARIO")
+				inventario.append("MOVIL")
+			
 		elif respuesta==("MIRAR CASETTE"):
 			print("En la portada hay una ilustracion muy bonita, sobre esta, resalta el nombre del juego, 'La Busqueda' ")
 		elif respuesta==("ABRIR CASETTE"):
@@ -260,7 +381,7 @@ def dormitorio():
 			dormir=input("SI/NO>")
 			if dormir == ("SI"):
 				dia+=1
-				print("Has dormido, hoy es el dia " + str(dia))
+				print("Has dormido, hoy es el dia " + str(dia))			
 			elif dormir!=("NO") and dormir!=("SI"):
 				print("No te entiendo")
 		elif respuesta==("MIRAR TRAJE"):
@@ -275,6 +396,8 @@ def dormitorio():
 			inv()
 		elif respuesta==("ESTADO"):
 			estado()
+		elif respuesta==("PROCASTINAR"):
+			procastinar()
 		elif respuesta==("DESCRIPCION"):
 			print ("Estas en tu habitacion, hay una MESITA de noche, un ARMARIO, una CAMA, y una puerta hacia el SALON ")
 		else:
@@ -290,11 +413,11 @@ print ("La Busqueda, La Aventura")
 print ("========================")
 print ("")
 print ("Pipipipipipi")
-print ("Son las 6:30 de la mañana, ha sido una noche muy calurosa y has sudado como si fuera Écija a las 12 de la tarde en Agosto")
+print ("Son las 6:00 de la mañana, ha sido una noche muy calurosa y has sudado como si fuera Écija a las 12 de la tarde en Agosto")
 print ("El DESPERTADOR no deja de sonar")
 print ("¿Qué quieres hacer?")
 print ("Los comandos han de introducirse EN MAYUSCULAS")
-respuesta=input(">")
+oficina()
 while respuesta!=("SEGUIR DURMIENDO"):
 	if respuesta == ("APAGAR DESPERTADOR"):
 		dormitorio()
